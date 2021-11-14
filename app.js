f2cda9ea4bd279ee7c67f8b4a297656da24c6de9
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-14 11:49:59 +0300

    getElements

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-let val;
+let taskTitle;
+//document.getElementById()
+taskTitle = document.getElementById("task-title");
 
-//Document Object
-val = document;
-val = document.head;
-val = document.body;
+//get from element
+val = taskTitle.id;
+val = taskTitle.className;
 
-val = document.images
+//change styling
+taskTitle.style.background = "#333";
+taskTitle.style.color = "#fff";
+taskTitle.style.padding = "5px";
+// document.getElementById("taskt-title").style.display = "none";
+
+//change content
+taskTitle.textContent = "Task List";
+taskTitle.innerText = "My Tasks";
+taskTitle.innerHTML = "<span style='color:red'> Task List </span>";
 console.log(val);
+
+//document.querySelector()
+let val2;
+
+val2 = document.querySelector("h5"); //get first occurrence
+val2 = document.querySelector("h5:nth-child(3)"); //get 3th occurrence
+
+console.log(val2);
