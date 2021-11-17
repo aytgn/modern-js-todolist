f1e8feb675313d7bedb900f1d62da3ada4b06f30
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-17 11:21:38 +0300

    local storage manipluation

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-//EVENT BUBBLING
+// //set local storage item
+// localStorage.setItem("name", "john");
 
-// const cardTitle = document.querySelector(".card-title");
-// cardTitle.addEventListener("click", runEvent.bind(cardTitle, "Card Title"));
+// //set session storage item
+// sessionStorage.setItem("name", "Beth");
 
-// const cardContent = document.querySelector(".card-content");
-// cardContent.addEventListener(
-//   "click",
-//   runEvent.bind(cardContent, "Card Content")
-// );
+// //remove local storage item
+// // localStorage.removeItem("name");
 
-// const card = document.querySelector(".card");
-// card.addEventListener("click", runEvent.bind(card, "Card"));
+// //get from storage
+// let userName = localStorage.getItem("name");
+// console.log(userName);
 
-// function runEvent(text) {
-//   console.log(text);
-// }
+// //clear local storage
+// localStorage.clear();
 
-//EVENT DELEGATION
-// const deleteItem = document.querySelector(".delete-item ");
-// deleteItem.addEventListener("click", delItem);
+// userName = localStorage.getItem("name");
+// console.log(userName);
 
-const body = document.querySelector("body");
-body.addEventListener("click", delItem);
+//get control of taskInput and taskButton
+const taskInput = document.querySelector("input#task");
+const TaskBtn = document.querySelector("div.card-content input.btn");
 
-function delItem(event) {
-  console.log(event.target); //has event already!!
-  if (event.target.parentElement.classList.contains("delete-item")) {
-    console.log("delete item");
-    event.target.parentElement.parentElement.remove();
-  }
-}
+//taskButton will trigger whenever clicked
+//taskButton will do these:
+/**
+ * 1)sync local tasks and dom tasks
+ *  1.1)parse JSON to string to get local values
+ * 2)assign input value to the dom task
+ * 3)push dom task to dom tasks
+ * 4)assign dom tasks to the local tasks
+ *  4.1)parse string to JSON to assign local values
+ * 5)get local values
+ */
+
+//for simplicity sake lets start with not empty tasks array
+localStorage.setItem("tasks", ["wash these dishes", "pet the cat"]);
+
+TaskBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const tasksArray = localStorage.getItem("tasks").split(","); // 1
+  let task;
+  task = taskInput.value; //2
+  tasksArray.push(task); //3
+  localStorage.setItem("tasks", tasksArray); //4
+  console.log(localStorage.getItem("tasks"));
+});
