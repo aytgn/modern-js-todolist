6e9ab5d2538e16c19e78cd4f3ce14dc64fd3f2d2
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-17 09:45:07 +0300

    mouse events

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-const onClick = function (str) {
+const clearBtn = document.querySelector(".clear-tasks");
+const card = document.querySelector(".card");
+const heading = document.querySelector("h5");
+
+//Click
+// clearBtn.addEventListener("click", runEvent.bind("the Text!"));
+
+//Double Click
+
+// clearBtn.addEventListener("dbClick", runEvent);
+
+//Mouse Down
+// clearBtn.addEventListener("mousedown", runEvent.bind("text"));
+
+//Mouse Up
+// clearBtn.addEventListener("mouseup", runEvent.bind("text"));
+
+//Mouse Enter
+// clearBtn.addEventListener("mouseenter", runEvent);
+
+//Mouse Leave
+// clearBtn.addEventListener("mouseleave", runEvent);
+
+//Mouse Over
+// clearBtn.addEventListener("mouseover", runEvent);
+
+//Mouse Out
+// clearBtn.addEventListener("mouseout", runEvent);
+
+//Mouse Move
+clearBtn.addEventListener("mousemove", runEvent);
+
+//Event Handler
+function runEvent(text) {
   event.preventDefault();
-  console.log("hello");
-  console.log(str);
-};
-
-document
-  .querySelector(".clear-tasks")
-  .addEventListener("click", onClick.bind("aga")); //event default argument!
-
-  
+  console.log(text);
+  console.log(`EVENT TYPE ${event.type} `);
+}
